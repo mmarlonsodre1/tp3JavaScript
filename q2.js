12f686147cce1d3253e92b49578fa9a591264865
Marlon Sodré de Souza <marlon.sodre@al.infnet.edu.br>
2019-11-14 09:00:38 -0300

    q2 e q3

--- q2.js
+++ q2.js
@@ -1,8 +1,29 @@
+mydiv = document.getElementById('q2');
+
+input = document.createElement("INPUT");
+label = document.createElement("P");
+button = document.createElement("BUTTON")
+
+input.setAttribute('type', 'number');
+input.setAttribute('placeholder', 'fatorial');
+input.required = true;
+
+button.setAttribute('onclick', 'calcular()');
+button.appendChild(document.createTextNode('calcular'));
+
+mydiv.appendChild(input);
+mydiv.appendChild(button);
+mydiv.appendChild(label);
+
 function fatorial(number) {
     if (number == 1) return 1;
     return number * fatorial(number - 1);
 }
 
-console.time('tempo');
-console.log(`53! = ${fatorial(53)}`);
-console.timeEnd('tempo');
+function calcular() {
+    label.innerHTML = "";
+    if (input.value > 0) {
+        let inicio = new Date();
+        label.appendChild(document.createTextNode(`${input.value}! = ${fatorial(input.value)} (${new Date() - inicio} milisegundos)`));
+    }
+}
--- q3.js
+++ q3.js
@@ -1,22 +1,49 @@
+mydiv = document.getElementById('q3');
+
+titulo = document.createElement("P");
+aprovado = document.createElement("P");
+rejeitados = document.createElement("P");
+rodape = document.createElement("P");
+button = document.createElement("BUTTON")
+
+button.setAttribute('onclick', 'relatorio()');
+button.appendChild(document.createTextNode('relatorio'));
+
+mydiv.appendChild(button);
+mydiv.appendChild(titulo);
+mydiv.appendChild(aprovado);
+mydiv.appendChild(rejeitados);
+mydiv.appendChild(rodape);
+
 var arrayAlunos = [];
 var aprovados = 0;
 var reprovados = 0;
 
-for (let i = 1; i <= 20; i++) {
-    arrayAlunos.push({
-        numero: i,
-        nota: Math.round(Math.random() * 100)
-    });
-}
 
-for (const aluno of arrayAlunos) {
-    if (aluno.nota >= 50) {
-        aprovados++;
-        console.log(`Aluno nr ${aluno.numero} - Nota ${aluno.nota} [APROVADO]`);
-    } else {
-        reprovados++;
-        console.log(`Aluno nr ${aluno.numero} - Nota ${aluno.nota} [REPROVADO]`);
+function relatorio() {
+    titulo.innerHTML = "";
+    aprovados.innerHTML = "";
+    rejeitados.innerHTML = "";
+    rodape.innerHTML = "";
+
+    titulo.appendChild(document.createTextNode('Meu Relatorio'));
+    for (let i = 1; i <= 20; i++) {
+        arrayAlunos.push({
+            numero: i,
+            nota: Math.round(Math.random() * 100)
+        });
     }
+
+    for (const aluno of arrayAlunos) {
+        if (aluno.nota >= 50) {
+            aprovados++;
+            aprovado.appendChild(document.createTextNode(`Aluno nr ${aluno.numero} - Nota ${aluno.nota} [APROVADO]`));
+            aprovado.appendChild(document.createElement('BR'));
+        } else {
+            reprovados++;
+            rejeitados.appendChild(document.createTextNode(`Aluno nr ${aluno.numero} - Nota ${aluno.nota} [REPROVADO]`));
+            rejeitados.appendChild(document.createElement('BR'));
+        }
+    }
+    rodape.appendChild(document.createTextNode(`APROVADOS ${(aprovados / arrayAlunos.length) * 100}% | REPROVADOS ${(reprovados / arrayAlunos.length) * 100}%`));
 }
-console.log(`APROVADOS ${aprovados} - ${(aprovados / arrayAlunos.length) * 100}%`);
-console.log(`REPROVADOS ${reprovados} - ${(reprovados / arrayAlunos.length) * 100}%`);
